3d8b22722294a4ee35c6b85ee65048dffba2104f
Saiyajin22 <tothzoltan0713@gmail.com>
2023-10-11 19:25:09 +0200

    block data refactored

--- src/main/java/org/bitcoin/mining/to/sat/data/BlockData.java
+++ src/main/java/org/bitcoin/mining/to/sat/data/BlockData.java
@@ -7,21 +7,18 @@ import java.util.ArrayList;
 
 public class BlockData {
     public static Block getBlock100500ThExample() {
-//        return new Block(
-//                438,
-//                new BlockHeader(
-//                        1,
-//                        "000000000001b2cdb438f6324a9311fae34aceff519333d1d11164ddaa87a409", // The 100499Th BTC block hash
-//                        "9ac2659ba7ad885813586c2f47e3c3ad0987b31f974c8669a130ae753a43495c",
-//                        1293883796,
-//                        "1fffffff", // This differs from the actual bits which was: 0x1b04864c
-//                        0L // Not sure if it's right, but nonce will change all the time so the initial shouldn't matter
-//                ),
-//                2,
-//                new ArrayList<>()
-//        );
         return new Block(
-                0
+                438,
+                new BlockHeader(
+                        1,
+                        "000000000001b2cdb438f6324a9311fae34aceff519333d1d11164ddaa87a409", // The 100499Th BTC block hash
+                        "9ac2659ba7ad885813586c2f47e3c3ad0987b31f974c8669a130ae753a43495c",
+                        1293883796,
+                        "1fffffff", // This differs from the actual bits which was: 0x1b04864c
+                        0L // Not sure if it's right, but nonce will change all the time so the initial shouldn't matter
+                ),
+                2,
+                new ArrayList<>()
         );
     }
 
