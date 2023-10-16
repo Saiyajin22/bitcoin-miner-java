7470d8b9c4ff27dad146021afe2afa073ef0e62c
Saiyajin22 <tothzoltan0713@gmail.com>
2023-10-16 11:13:48 +0200

    good jbmc run

--- src/main/java/org/bitcoin/mining/to/sat/Main.java
+++ src/main/java/org/bitcoin/mining/to/sat/Main.java
@@ -1,10 +1,27 @@
 package org.bitcoin.mining.to.sat;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.bitcoin.mining.to.sat.data.BlockData;
-import org.bitcoin.mining.to.sat.service.Miner;
+import org.bitcoin.mining.to.sat.model.Block;
+
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        Miner.mineSAT(BlockData.getBlock100500ThExample());
+        final Block block = BlockData.getBlock100500ThExample();
+
+        long nonce = 510;
+        int flag = 0;
+
+        block.getHeader().setNonce(nonce);
+
+        final String sha256hex = DigestUtils.sha256Hex(DigestUtils.sha256Hex(block.getHeader().toString()));
+
+        // Here we check if the hash is valid
+        if (Objects.nonNull(sha256hex)) {
+            flag = 1;
+        }
+
+        assert (flag == 0);
     }
 }
