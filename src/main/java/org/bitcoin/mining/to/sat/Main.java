1f55c80cbe8eead02cdd0b681ac9fb770b255a48
Saiyajin22 <tothzoltan0713@gmail.com>
2023-10-23 13:54:00 +0200

    modify main to involve loop

--- src/main/java/org/bitcoin/mining/to/sat/Main.java
+++ src/main/java/org/bitcoin/mining/to/sat/Main.java
@@ -4,24 +4,25 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.bitcoin.mining.to.sat.data.BlockData;
 import org.bitcoin.mining.to.sat.model.Block;
 
-import java.util.Objects;
-
 public class Main {
     public static void main(String[] args) {
         final Block block = BlockData.getBlock100500ThExample();
 
-        long nonce = 510;
-        int flag = 0;
+        long nonce = 499;
+        for (int i = 500; i < 515; i++) {
+            nonce++;
+            int flag = 0;
 
-        block.getHeader().setNonce(nonce);
+            block.getHeader().setNonce(nonce);
 
-        final String sha256hex = DigestUtils.sha256Hex(DigestUtils.sha256Hex(block.getHeader().toString()));
+            final String sha256hex = DigestUtils.sha256Hex(DigestUtils.sha256Hex(block.getHeader().toString()));
 
-        // Here we check if the hash is valid
-        if (Objects.nonNull(sha256hex)) {
-            flag = 1;
+            // Here we check if the hash is valid
+            if (sha256hex.charAt(0) == '0' && sha256hex.charAt(1) == '0') {
+                flag = 1;
+            }
+
+            assert (flag == 0);
         }
-
-        assert (flag == 0);
     }
 }
